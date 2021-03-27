f321e73705fe6ca17837d305f00e1bee7e238cec
Santheep kumar <santheep.v@skematix.in>
2021-03-27 20:47:50 +0530

    used useReducer

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,23 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const reducer = (state, action) => {
+  if (action.type === "plus") {
+    return state + 1;
+  } else {
+    return state - 1;
+  }
+  // return state;
+};
 
 function Counter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
+
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>Plus</button>
+      <button onClick={() => dispatch({ type: "plus" })}>Plus</button>
       <h1>Count - {count}</h1>
-      <button onClick={() => setCount(count - 1)}>Minus</button>
+      <button onClick={() => dispatch({ type: "minus" })}>Minus</button>
     </div>
   );
 }
