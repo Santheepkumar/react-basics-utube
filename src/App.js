01e9e0de84a667d8cd5c72b725158b761212270d
Santheep kumar <santheep.v@skematix.in>
2021-03-29 22:18:28 +0530

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import Counter from "./components/Counter";
 import Table from "./components/Table";
+import Memo from "./components/Memo";
 
 function App() {
   return (
     <div>
       {/* <Counter /> */}
-      <Table />
+      {/* <Table /> */}
+      <Memo />
     </div>
   );
 }
