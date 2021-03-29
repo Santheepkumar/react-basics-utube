b8e681c45ae03ac5b314e25fb9cbb48004bd7575
Santheep kumar <santheep.v@skematix.in>
2021-03-29 23:35:07 +0530

    React router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-// import Counter from "./components/Counter";
-import Table from "./components/Table";
-import Memo from "./components/Memo";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import About from "./components/About";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div>
-      {/* <Counter /> */}
-      {/* <Table /> */}
-      <Memo />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about/12345'>About</Link>
+          </li>
+          <li>
+            <Link to='/dashboard'>Dashboard</Link>
+          </li>
+        </ul>
+        <hr />
+
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/about/:id'>
+            <About />
+          </Route>
+          <Route path='/dashboard'>
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
