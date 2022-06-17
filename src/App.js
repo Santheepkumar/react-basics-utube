359d653f047684722f26455356f2387ecdf8e724
Santheep kumar <santheep@hod.digital>
2022-06-17 16:16:57 +0530

    React 18 - useTransition and useDeferredValue

--- src/App.js
+++ src/App.js
@@ -1,44 +1,89 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import About from "./components/About";
-import Dashboard from "./components/Dashboard";
-import Home from "./components/Home";
-import { userContext } from "./UserContext";
+// State and Rendering
+// useTransition
+// useTransition cons
+// useDeferredValue
+
+import React from "react";
+
+// function App() {
+//   const [input, setInput] = React.useState("");
+//   const [list, setList] = React.useState([]);
+//   const [isLoading, startTransition] = React.useTransition();
+
+//   const handleChange = (e) => {
+//     const value = e.target.value;
+//     setInput(value);
+
+//     const late = 20000;
+//     const listItems = [];
+//     for (let i = 0; i < late; i++) {
+//       listItems.push(value);
+//     }
+//     startTransition(() => {
+//       setList(listItems);
+//     });
+//   };
+
+//   console.log("rendered")
+
+//   return (
+//     <div className='max-w-7xl mx-auto mt-4'>
+//       <input
+//         className='bg-gray-200 text-green-500 border border-green-300 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-green-500'
+//         value={input}
+//         onChange={handleChange}
+//       />
+
+//       {isLoading ? (
+//         <div className='animate-bounce'>Loading..</div>
+//       ) : (
+//         <article>
+//           {list.map((item, i) => (
+//             <div className='p-4 bg-green-400 m-2 inline-block'>{item}</div>
+//           ))}
+//         </article>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
 
 function App() {
-  const [user, setUser] = useState({});
+  const [input, setInput] = React.useState("");
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setInput(value);
+  };
+
+  const deferredInput = React.useDeferredValue(input);
+
+  const list = React.useMemo(() => {
+    const late = 10000;
+    const listItems = [];
+    for (let i = 0; i < late; i++) {
+      listItems.push(deferredInput);
+    }
+    return listItems;
+  }, [deferredInput]);
+
+  console.log(`Input - ${input} || DeferredInput ${deferredInput}`);
+
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/about/12345'>About</Link>
-          </li>
-          <li>
-            <Link to='/dashboard'>Dashboard</Link>
-          </li>
-        </ul>
-        <hr />
+    <div className='max-w-7xl mx-auto mt-4'>
+      <input
+        className='bg-gray-200 text-green-500 border border-green-300 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-green-500'
+        value={input}
+        onChange={handleChange}
+      />
 
-        <Switch>
-          <userContext.Provider value={[user, setUser]}>
-            <Route exact path='/'>
-              <Home />
-            </Route>
-            <Route path='/dashboard'>
-              <Dashboard />
-            </Route>
-          </userContext.Provider>
-
-          <Route path='/about/:id'>
-            <About />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+      <article>
+        {list.map((item, i) => (
+          <div className='p-4 bg-green-400 m-2 inline-block'>{item}</div>
+        ))}
+      </article>
+    </div>
   );
 }
 
--- src/UserContext.js
+++
@@ -1,6 +0,0 @@
-import { createContext, useContext } from "react";
-
-const userContext = createContext("");
-const useUserContext = () => useContext(userContext);
-
-export { userContext, useUserContext };
